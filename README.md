279f8936fc3a9028b3c02483cd270a8c94f22577
Alexinaldo Costa <alex.costa@controlinformatica.com.br>
2019-12-28 07:14:35 -0300

    Commit inicial

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # pymaxdb
-Projeto para facilitar a comunicação com alguns bancos de dados
+Projeto com o intuito de facilitar a comunicação com alguns bancos de dados.
+
+## Objetivo
+
+## Instalação
+
+## Utilização
+
+
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open(file="README.md",  mode="r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name = 'pymaxdb',
+    version = '1.0.0',
+    author = 'Alexinaldo Costa',
+    author_email = 'ayronmax@gmail.com',
+    packages = find_packages(),
+    description = 'Projeto com o intuito de facilitar a comunicação com alguns bancos de dados.',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = 'https://github.com/ayronmax/pymaxdb',
+    project_urls = {
+        'Código fonte': 'https://github.com/ayronmax/pymaxdb',
+        'Download': 'https://github.com/ayronmax/pymaxdb/archive/master.zip'
+    },
+    license = 'MIT',
+    keywords = 'ODBC PostgreSQL DBMaker Firebird SQLServer',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Portuguese (Brazilian)',
+        'Operating System :: OS Independent',
+        'Topic :: Database'
+    ],
+    install_requires = [
+        'psycopg2',
+        'pyodbc==4.0.27',
+        'fdb'
+    ]    
+)
